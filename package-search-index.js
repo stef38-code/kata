5f390eb59961fed1bd84eeedb9ee5de0fe6792a0
b_stef <stephane.betton@gmail.com>
2022-01-28 18:29:46 +0100

    remove file

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.stephane.kata.morse"},{"l":"org.stephane.kata.morse.business"},{"l":"org.stephane.kata.morse.dicos"},{"l":"org.stephane.kata.morse.exceptions"},{"l":"org.stephane.kata.morse.register"}]
